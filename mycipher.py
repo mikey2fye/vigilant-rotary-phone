98e99fb7037a225501a73ff8b52bdf831e43199b
mikey2fye <104800390+mikey2fye@users.noreply.github.com>
2022-05-02 22:52:57 -0400

    Updated

--- mycipher.py
+++ mycipher.py
@@ -1,1 +1,35 @@
-
+shift = int(input('What is your cipher number? '))
+text = input('What is your message? ')
+output = ''
+punctuation = ['!', '?', '.', ',', ';']
+for items in text:
+    if ('A' <= items <= 'Z'):
+        overrun = ord(items) + shift
+        if (overrun > 90):
+            overrun = overrun - 26
+        output += chr(overrun)
+    elif ('a' <= items <= 'z'):
+        overrun = ord(items) + shift
+        if (overrun > 122):
+            overrun = overrun - 26
+        output += chr(overrun)
+    elif (items in punctuation or items == ' '):
+        pass
+    else:
+        output += chr(ord(items))
+format_output = ''
+cols = 0
+rows = 0
+for char in output:
+    if cols == 5:
+        format_output += ' '
+        cols = 0
+        rows += 1
+    if rows == 10:
+        format_output += '\n'
+        rows = 0
+    format_output += char
+    cols += 1
+print(format_output.upper())
+                
+   
